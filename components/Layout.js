64c6f12ce8e19a59c5de181cf8ccf2088c3ec7cd
Bruno Henrique Paiva <bruno@sigane.com.br>
2020-10-25 05:36:15 -0300

    Moved meta, link and title tags to custom app component

--- components/Layout.js
+++ components/Layout.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import Head from "next/head";
 
-export default function Layout({ children }) {
-  return(
-    <div>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <link
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
-        />
-        <title>My Portfolio</title>
-      </Head>
-      <div className="lg:flex lg:items-center lg:justify-center lg:h-screen lg:mx-auto">
-        <div className="px-5 py-5">
-          { children }
-        </div>
+export default function Layout({children}) {
+  return (
+    <div className="lg:flex lg:items-center lg:justify-center lg:h-screen lg:mx-auto">
+      <div className="px-5 py-5">
+        {children}
       </div>
     </div>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import Head from "next/head";
 
 import '../styles/index.css'
 import "../styles/index.scss";
 
-function App({ Component, pageProps }) {
+function App({Component, pageProps}) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <title>My Portfolio</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
 }
 
